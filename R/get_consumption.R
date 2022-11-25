bc16813cd1ff523da47519e2494ca50cb47cc5c5
James McMahon <jamesmcmahon0@gmail.com>
2022-11-25 10:58:07 +0000

    Use `get_api_key` in api functions

--- R/get_consumption.R
+++ R/get_consumption.R
@@ -1,10 +1,8 @@
 #' Get consumption data
 #'
-#' @param api_key
-#' @param mpan
-#' @param serial_number
-#' @param period_from
-#' @param period_to
+#' @inheritParams octopus_api
+#' @param mpan,serial_number
+#' @param period_from,period_to
 #' @param page_size
 #' @param order_by
 #' @param group_by
@@ -12,9 +10,10 @@
 #' @return
 #' @export
 get_consumption <-
-  function(api_key,
+  function(
            mpan,
            serial_number,
+    api_key = get_api_key(),
            period_from = NULL,
            period_to = NULL,
            page_size = 100L,
