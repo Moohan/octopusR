6515e0abb329a5266bfa606a14e2d56780259ce9
James McMahon <james.mcmahon@gmail.com>
2022-11-26 18:33:56 +0000

    Add meter type parameter to choose between electric or gas

--- R/get_consumption.R
+++ R/get_consumption.R
@@ -12,32 +12,36 @@
 get_consumption <-
   function(mpan,
            serial_number,
+           meter_type = c("electric", "gas"),
            api_key = get_api_key(),
            period_from = NULL,
            period_to = NULL,
            page_size = 100L,
            order_by = c("-period", "period"),
            group_by = c("hour", "day", "week", "month", "quarter")) {
+    meter_type <- match.arg(meter_type)
     if (missing(group_by)) {
       group_by <- NULL
     } else {
       group_by <- match.arg(group_by)
     }
-
     if (missing(order_by)) {
       order_by <- NULL
     } else {
       order_by <- match.arg(order_by)
     }
 
+    path <- paste("/v1",
+      paste0(meter_type, "-meter-points"),
+      mpan,
+      "meters",
+      serial_number,
+      "consumption/",
+      sep = "/"
+    )
+
     resp <- octopus_api(
-      path = paste("/v1/electricity-meter-points",
-        mpan,
-        "meters",
-        serial_number,
-        "consumption/",
-        sep = "/"
-      ),
+      path = path,
       api_key = api_key,
       query = list(
         period_from = period_from,
