b283c3f80b298cce77141fc4bd73fba95dd96b64
James McMahon <james.mcmahon@gmail.com>
2022-12-03 23:08:31 +0000

    Add tests for `get_consumption`

--- tests/testthat/test-get_consumption.R
+++ tests/testthat/test-get_consumption.R
@@ -1,5 +1,27 @@
-test_that("Can return consumption data", {
-  consumption_data <- get_consumption("electricity")
+test_that("Can return consumption data sample", {
+  expect_message(
+    {
+      consumption_data <- get_consumption("electricity")
+    },
+    "Returning 100 rows only as a date range wasn't provided"
+  )
 
   expect_s3_class(consumption_data, "tbl")
+  expect_named(
+    consumption_data,
+    c("consumption", "interval_start", "interval_end")
+  )
+  expect_identical(nrow(consumption_data), 100L)
+})
+
+test_that("errors properly with incorrect params", {
+  expect_error(
+    get_consumption(),
+    "You must specify \"electricity\" or \"gas\" for `meter_type`"
+  )
+
+  expect_error(
+    get_consumption("electricity", period_to = Sys.Date()),
+    "To use `period_to` you must also provide the `period_from` parameter to create a range"
+  )
 })
