bb15122d81b1267e28f9139c33ba24d1f2896b2c
James McMahon <james.mcmahon@gmail.com>
2022-12-03 23:55:07 +0000

    Allow requests without api_key authentication

--- R/octopus_api.R
+++ R/octopus_api.R
@@ -5,14 +5,22 @@
 #' @inheritParams set_api_key
 #'
 #' @return An Octopus API object
-octopus_api <- function(path, query = NULL, api_key = get_api_key()) {
-  req <- httr2::request(glue::glue("https://{api_key}@api.octopus.energy/")) |>
+octopus_api <- function(path, query = NULL, api_key = NULL, use_api_key = FALSE) {
+  if (use_api_key || !missing(api_key)) {
+    if (missing(api_key)) api_key <- get_api_key()
+
+    base_url <- glue::glue("https://{api_key}@api.octopus.energy/")
+  } else {
+    base_url <- "https://api.octopus.energy/"
+  }
+
+  req <- httr2::request(base_url) |>
     httr2::req_user_agent("octopusR (https://github.com/Moohan/octopusR)") |>
     httr2::req_url_path_append(path) |>
     httr2::req_url_query(!!!query)
 
   resp <- req |>
-    httr2::req_throttle(1L) |>
+    httr2::req_throttle(5 / 1) |>
     httr2::req_error(body = octopus_error_body) |>
     httr2::req_perform()
 
@@ -31,5 +39,7 @@ octopus_api <- function(path, query = NULL, api_key = get_api_key()) {
 }
 
 octopus_error_body <- function(resp) {
-  httr2::resp_body_json(resp)[["detail"]]
+  resp |>
+    httr2::resp_body_json() |>
+    magrittr::extract2("detail")
 }
--- tests/testthat/test-octopus_api.R
+++ tests/testthat/test-octopus_api.R
@@ -1,8 +1,14 @@
 test_that("Octopus API fails when not authenticated", {
   expect_error(
     octopus_api(path = "v1/accounts/"),
+    "Authentication credentials were not provided\\.$"
+  )
+
+  expect_error(
+    octopus_api(path = "v1/accounts/", api_key = get_api_key()),
     "You do not have permission to perform this action\\.$"
   )
+
   expect_error(
     octopus_api(path = "v1/accounts/", api_key = "incorrect_api_key"),
     "Invalid API key\\.$"
