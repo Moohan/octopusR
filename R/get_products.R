dc5d899a685a8fc68a501dfb32588f8349f3c4f4
Moohan <Moohan@users.noreply.github.com>
2022-12-10 18:29:19 +0000

    Style code

--- R/get_products.R
+++ R/get_products.R
@@ -18,34 +18,40 @@ get_products <- function(is_variable = NULL,
   if (!missing(is_prepay)) {
     check_logical(is_prepay, "is_prepay")
   }
-    check_logical(is_business, "is_business")
-    check_datetime_format(available_at, "available_at")
+  check_logical(is_business, "is_business")
+  check_datetime_format(available_at, "available_at")
 
   path <- "/v1/products/"
 
-  query <- list(is_variable = is_variable,
-                is_green = is_green,
-                is_tracker = is_tracker,
-                is_prepay = is_prepay,
-                is_business = is_business,
-                available_at = available_at)
+  query <- list(
+    is_variable = is_variable,
+    is_green = is_green,
+    is_tracker = is_tracker,
+    is_prepay = is_prepay,
+    is_business = is_business,
+    available_at = available_at
+  )
 
   check_logical(authenticate, "authenticate")
 
-  if(authenticate) {
+  if (authenticate) {
     if (missing(api_key)) {
       api_key <- get_api_key()
     }
-    resp <- octopus_api(path = path,
-                        query = query,
-                        use_api_key = TRUE,
-                        api_key = api_key)
-} else {
-  resp <- octopus_api(path = path,
-                      query = query)
+    resp <- octopus_api(
+      path = path,
+      query = query,
+      use_api_key = TRUE,
+      api_key = api_key
+    )
+  } else {
+    resp <- octopus_api(
+      path = path,
+      query = query
+    )
   }
 
- products <- resp[["content"]][["results"]]
+  products <- resp[["content"]][["results"]]
 
- return(products)
+  return(products)
 }
