33578f13b4748075fd139b4c4b052394422e5d0b
James McMahon <james.mcmahon@gmail.com>
2022-12-11 21:04:23 +0000

    Add documentation

--- R/get_products.R
+++ R/get_products.R
@@ -1,3 +1,24 @@
+#' Return a list of energy products
+#'
+#' @description By default, results will be public energy products but if
+#'  authenticated organisations will also see products available to
+#'  their organisation.
+#'
+#' @param is_variable (boolean, optional) Show only variable products.
+#' @param is_green (boolean, optional) Show only green products.
+#' @param is_tracker (boolean, optional) Show only tracker products.
+#' @param is_prepay (boolean, optional) Show only pre-pay products.
+#' @param is_business (boolean, default: FALSE) Show only business products.
+#' @param available_at Show products available for new agreements on the given datetime.
+#' Defaults to current datetime, effectively showing products that are currently available.
+#' @param authenticate (boolean, default: FALSE) Use an `api_key` to authenticate. Only useful for organisations.
+#' @inheritParams set_api_key
+#'
+#' @return a [tibble][tibble::tibble-package]
+#' @export
+#'
+#' @examples
+#' get_products(is_green = TRUE)
 get_products <- function(is_variable = NULL,
                          is_green = NULL,
                          is_tracker = NULL,
