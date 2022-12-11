d00561e65e423b97d63c56610fa6ea19418af50b
James McMahon <james.mcmahon@gmail.com>
2022-12-11 20:28:59 +0000

    Add better tests for `check_params`

--- tests/testthat/test-check_params.R
+++ tests/testthat/test-check_params.R
@@ -1,6 +1,14 @@
 test_that("Works for correct dates", {
-  expect_identical(
-    check_datetime_format("2022-01-01", NULL),
-    "2022-01-01"
+  expect_invisible(
+    check_datetime_format("2022-01-01"),
+  )
+  expect_invisible(
+    check_datetime_format("2022-01-01 00:00"),
+  )
+})
+
+test_that("Errors on incorrect dates", {
+  expect_error(
+    check_datetime_format("01/01/2022"),
   )
 })
