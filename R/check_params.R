653d43c8ab2abbd3e1e7b8e970ee38c8e6feb9a5
James McMahon <james.mcmahon@gmail.com>
2022-12-11 21:04:09 +0000

    Fix issues

--- R/check_params.R
+++ R/check_params.R
@@ -1,4 +1,10 @@
 check_datetime_format <- function(arg_date, arg = rlang::caller_arg(arg_date), call = rlang::caller_env()) {
+  if (!inherits(arg_date, "character") && !inherits(arg_date, "Date")) {
+    cli::cli_abort("{.arg {arg}} must be {.cls character} or {.cls Date}, not {.cls {class(arg_date)}}.",
+                   call = call
+    )
+  }
+
   iso_8601_pattern <- "^([\\+-]?\\d{4}(?!\\d{2}\\b))((-?)((0[1-9]|1[0-2])(\\3([12]\\d|0[1-9]|3[01]))?|W([0-4]\\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\\d|[12]\\d{2}|3([0-5]\\d|6[1-6])))([T\\s]((([01]\\d|2[0-3])((:?)[0-5]\\d)?|24\\:?00)([\\.,]\\d+(?!:))?)?(\\17[0-5]\\d([\\.,]\\d+)?)?([zZ]|([\\+-])([01]\\d|2[0-3]):?([0-5]\\d)?)?)?)?$"
 
   if (!grepl(iso_8601_pattern, arg_date, perl = TRUE)) {
@@ -11,26 +17,10 @@ check_datetime_format <- function(arg_date, arg = rlang::caller_arg(arg_date), c
   }
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Improve `check_logical`
 check_logical <- function(arg_lgl, arg = rlang::caller_arg(arg_lgl), call = rlang::caller_env()) {
-  if (class(arg_lgl) != "logical") {
-    cli::cli_abort("{.arg {arg}} must be {.cls logical}, not {.cls {class(param_lgl)}}.",
+  if (!inherits(arg_lgl, "logical")) {
+    cli::cli_abort("{.arg {arg}} must be {.cls logical}, not {.cls {class(arg_lgl)}}.",
       call = call
     )
-<<<<<<< HEAD
   }
-=======
-check_logical <- function(param_lgl, param_name) {
-  if (class(param_lgl) != "logical") {
-    cli::cli_abort("{.arg {param_name}} must be {.cls logical}, not {.cls {class(param_lgl)}}.")
-  }
-
-  return(param_lgl)
->>>>>>> Implement products API endpoint
-=======
-  }
->>>>>>> Improve `check_logical`
 }
