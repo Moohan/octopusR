1f4a8f9155eca77e4d9cc629dc8bdb941ce7100a
Moohan <Moohan@users.noreply.github.com>
2023-02-19 18:47:53 +0000

    Style code

--- R/meter_details.R
+++ R/meter_details.R
@@ -67,9 +67,9 @@ get_meter_details <-
       return(meter)
     }
 
-      cli::cli_abort(
-        "Meter details were missing or incomplete, please supply with {.arg mpan_mprn} and {.arg serial_number} arguments or with {.help [{.fun set_meter_details}](octopusR::set_meter_details)}"
-      )
+    cli::cli_abort(
+      "Meter details were missing or incomplete, please supply with {.arg mpan_mprn} and {.arg serial_number} arguments or with {.help [{.fun set_meter_details}](octopusR::set_meter_details)}"
+    )
   }
 
 testing_meter <- function(meter_type = c("electricity", "gas")) {
