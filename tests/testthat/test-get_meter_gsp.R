4f610f2e1ef5a98e6c5197d2b69066f9372b3c97
James McMahon <james.mcmahon@gmail.com>
2023-02-19 12:59:05 +0000

    Style

--- tests/testthat/test-get_meter_gsp.R
+++ tests/testthat/test-get_meter_gsp.R
@@ -1,10 +1,14 @@
 test_that("Can get a meter GSP", {
   test_meter <- testing_meter("electricity")
-  expected_gsp <- httr2::secret_decrypt("5GkfdUf-Fp88BMOFir1kkOOl",
-                                        "OCTOPUSR_SECRET_KEY")
+  expected_gsp <- httr2::secret_decrypt(
+    "5GkfdUf-Fp88BMOFir1kkOOl",
+    "OCTOPUSR_SECRET_KEY"
+  )
 
-  expect_equal(get_meter_gsp(mpan = test_meter[["mpan_mprn"]]),
-               expected_gsp)
+  expect_equal(
+    get_meter_gsp(mpan = test_meter[["mpan_mprn"]]),
+    expected_gsp
+  )
 })
 
 test_that("Fails with bad mprn", {
